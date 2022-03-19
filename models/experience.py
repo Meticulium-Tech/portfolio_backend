5c6a3796662f060c0d7d3f393bec75445c6da05d
baimamboukar <baimamboukar@gmail.com>
2022-03-19 21:11:06 +0100

    experience model

---
+++ models/experience.py
@@ -0,0 +1,6 @@
+class Experience:
+    def __init__(self, category, desc, title, comments):
+        self.type = category
+        self.desc = desc
+        self.title = title
+        self.comments = comments
